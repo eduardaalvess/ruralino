87d3af5ec6060bde34934e84950a0aec3553f195
saintravi <Dudah4@gmail.com>
2021-07-17 22:42:09 -0300

    pag inicial form

---
+++ src/sample/PagInicialController.java
@@ -0,0 +1,23 @@
+package sample;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class PagInicialController{
+
+    @FXML
+    private Button pessoasButton;
+
+    @FXML
+    private Button gruposButton;
+
+    @FXML
+    private Button presentesButton;
+
+    @FXML
+    private Button sorteioButton;
+
+    @FXML
+    private Button sairButton;
+
+}
